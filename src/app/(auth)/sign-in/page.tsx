c1ec09fd7d0a8cb15c8ce63ff93e1246dfc4c1a2
SatishK2022 <sk7399052@gmail.com>
2024-08-13 20:17:50 +0530

    add sign-up page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,23 +1,7 @@
-'use client'
+import React from 'react'
 
-import { useSession, signIn, signOut } from "next-auth/react"
-
-export default function SignIn() {
-    const { data: session } = useSession()
-
-    if (session) {
-        return (
-            <div>
-                Signed in as {session.user.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-        </div>
-    )
+export default function Sign_In() {
+  return (
+    <div>Sign_In</div>
+  )
 }
